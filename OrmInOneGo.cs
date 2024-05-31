06bf8ad96420641aa8b53349afa28e9f7d756f7f
Dmitriy Sukharev <dmitriy.sukharev@fullhpltd.com>
2024-05-31 16:18:11 +0300

    Initial commit

---
+++ OrmInOneGo.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class Orm
+{
+    private Database database;
+
+    public Orm(Database database) => this.database = database;
+
+    public void Write(string data)
+    {
+        using (database)
+        {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+        }
+                   
+    }
+
+    public bool WriteSafely(string data)
+    {
+        using (database)
+        try
+        {
+            Write(data);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
